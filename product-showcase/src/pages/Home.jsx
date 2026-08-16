17203a4c5c9c356976affea6ffa021c18a6a3edc
Imkumara <waynemark.ouma@student.moringaschool.com>
2026-08-16 16:32:19 +0300

    Add Home and Admin pages with product CRUD

---
+++ product-showcase/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Snack Shark</h1>
+      <p>
+        Snack Shark is your one-stop admin portal for managing our snack
+        inventory. Browse the shop to see what's available, or head to the
+        admin dashboard to add new snacks and update prices.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
